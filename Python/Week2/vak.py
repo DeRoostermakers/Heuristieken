c2a87ff001ce36fe463d4008a98dc5f3b0fd5109
LinseyUvA <linseyschaap@tele2.nl>
2018-04-17 11:36:01 +0200

    update vak.py

--- Python/Week2/vak.py
+++ Python/Week2/vak.py
@@ -1,21 +1,32 @@
 import csv
 
+vakkenlijst = []
+
 with open('vakken.csv') as csvFile:
     readCSV = csv.reader(csvFile, delimiter=';')
+    print(readCSV)
+
+
+
+
+    class Vak(object):
+        """docstring for [object Object]."""
+        def __init__(self, id, naam, hc, wc, maxWc, prac, maxPrac):
+            self.id = id
+            self.naam = naam
+            self.hc = hc
+            self.wc =  wc
+            self.maxWc = maxWc
+            self.prac = prac
+            self.maxPrac = maxPrac
+            self.aantalStudenten = 0
+            self.studenten = []
+
+        def __str__(self):
+            return self.naam
 
     for row in readCSV:
-        print(row)
+        vakkenlijst.append(Vak(1, row[0], row[1], row[2], row[3], row[4], row[5]))
 
-
-class Vak(object):
-    """docstring for [object Object]."""
-    def __init__(self, id, naam, hc, wc, maxWc, prac, maxPrac, aantalStudenten):
-        self.id = id
-        self.naam = naam
-        self.hc = hc
-        self.wc =  wc
-        self.maxWc = maxWc
-        self.prac = prac
-        self.maxPrac = maxPrac
-        self.aantalStudenten = aantalStudenten
-        self.studenten = []
+    for vak in vakkenlijst:
+        print(vak)
