b56b3775ed9ee3811abed55b9096fd812f13e376
NadjaUvA <31736612+NadjaUvA@users.noreply.github.com>
2018-04-17 11:51:14 +0200

    Update studentenlijst Nadja

--- Python/Student.py
+++ Python/Student.py
@@ -1,15 +1,25 @@
 import csv
 
 class Student(object):
-    def __init__(self, achternaam, voornaam, studentnummer):
+    def __init__(self, achternaam, voornaam, studentnummer, vakken):
         self.achternaam = achternaam
         self.voornaam = voornaam
         self.studentnummer = studentnummer
-        self.vakken = []
+        self.vakken = vakken
 
-with open('studentenenvakken.csv', 'rb', encoding='utf-8') as csvfile:
+studentenLijst = []
+
+with open('studentenenvakken.csv', 'r', encoding="latin-1") as csvfile:
     reader = csv.reader(csvfile, delimiter=",")
     for row in reader:
-        print(row)
+        studentVakken = []
+        for vak in row[3:]:
+            if vak != "":
+                studentVakken.append(vak) 
+        print(studentVakken)
+        studentenLijst.append(Student(row[0], row[1], row[2], studentVakken))
+
+# discard first row with Excel variable names
+studentenLijst = studentenLijst[1:]
 
 csvfile.close()
