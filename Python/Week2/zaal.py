2094e148c30c637993b37101ad972236025e9018
NadjaUvA <31736612+NadjaUvA@users.noreply.github.com>
2018-04-17 10:40:47 +0200

    Update object zaal Nadja

---
+++ Python/Week2/zaal.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 17 10:14:56 2018
+
+@author: Nadja
+"""
+
+class zaal(object):
+    def __init__(self, naam, capaciteit, inGebruik, vak):
+        self.naam = naam
+        self.capaciteit = capaciteit
+        self.inGebruik = inGebruik
+        self.vak = vak
+
+A104 = zaal("A1.04", 41, 0, 0)
+A106 = zaal("A1.06", 22, 0, 0)
+A108 = zaal("A108", 20, 0, 0)
+A110 = zaal("A1.10", 20, 0, 0)
+B0201 = zaal("B0.201", 56, 0, 0)
+C0110 = zaal("C0110", 117, 0, 0)
+C1112 = zaal("C1.112", 60, 0, 0)
