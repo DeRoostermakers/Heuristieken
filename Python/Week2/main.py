5cc6ac6d65515c494235e96bd7d32ba83a1cf98b
LinseyUvA <linseyschaap@tele2.nl>
2018-04-19 15:26:39 +0200

    avondslot toegevoegd

--- Python/Week2/main.py
+++ Python/Week2/main.py
@@ -16,37 +16,37 @@ zaalLijst = []
 vanVakNaarId = {}
 
 def main():
-    
+
     initialize()
 
     # dict om id van tijdsloten om te zetten naar tijd
-    idNaarTijdslot = {1 : "9:00-11:00", 2 : "11:00-13:00", 3 : "13:00-15:00", 4 : "15:00-17:00"}
+    idNaarTijdslot = {1 : "9:00-11:00", 2 : "11:00-13:00", 3 : "13:00-15:00", 4 : "15:00-17:00", 5 : "17:00-19:00"}
 
     # dict met alle zalen per dag
-    dag = {1 : zaalLijst[:], 2 : zaalLijst[:], 3 : zaalLijst[:], 4 : zaalLijst[:]}
+    dag = {1 : zaalLijst[:], 2 : zaalLijst[:], 3 : zaalLijst[:], 4 : zaalLijst[:], 5 : zaalLijst[:]}
 
     # weekrooster met tijdslots en zalen
     rooster = {"maandag" : copy.deepcopy(dag), "dinsdag" : copy.deepcopy(dag), "woensdag" : copy.deepcopy(dag), "donderdag" : copy.deepcopy(dag), "vrijdag" : copy.deepcopy(dag)}
 
     for vak in vakkenLijst:
         print(vak.id)
-    
+
 def initialize():
-    
-    # aanmaken van dict en vakkenlijst creëren    
+
+    # aanmaken van dict en vakkenlijst creëren
     counter = 0
-    
+
     # inlezen van CSV bestand
     with open('vakken.csv') as csvFile:
         readCSV = csv.reader(csvFile, delimiter=';')
         next(readCSV, None)
-    
+
         # rijen van CSV inlezen en vakobject aanmaken
         for row in readCSV:
             vakkenLijst.append(VakKlasse.Vak(counter, row[0], row[1], row[2], row[3], row[4], row[5]))
             vanVakNaarId[row[0]] = counter
             counter += 1
-    
+
     # inlezen van CSV bestand
     with open('studentenenvakken.csv', 'r', encoding="latin-1") as csvfile:
         reader = csv.reader(csvfile, delimiter=",")
@@ -57,21 +57,21 @@ def initialize():
                 if college != "":
                     studentVakken.append(college)
             studentenLijst.append(StudentKlasse.Student(row[0], row[1], row[2], studentVakken))
-    
+
     # vakken in studentenlijst met id voorzien
     for student in studentenLijst:
         temp = []
         for vak in student.vakken:
             temp.append(vanVakNaarId[vak])
         student.vakken = temp
-    
+
     # aantal studenten per vak vastleggen
     for vak in vakkenLijst:
         for student in studentenLijst:
             if vak.id in student.vakken:
                 vak.studenten.append(student.studentnummer)
         vak.aantalStudenten = len(vak.studenten)
-        
+
     with open('zalen.csv') as csvFile:
         readCSV = csv.reader(csvFile, delimiter=';')
         next(readCSV, None)
