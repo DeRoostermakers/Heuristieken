fbec88840c3bcb1da8c20cc1be97c43b58e0796d
LinseyUvA <linseyschaap@tele2.nl>
2018-04-24 13:10:49 +0200

    score functie

---
+++ Code/Classes/extraTijdslot.py
@@ -0,0 +1,16 @@
+"""
+Bestand met functie die de maluspunten voor het gebruik van een extra tijdslot
+
+Linsey Schaap (11036109), Kenneth Goei (11850701), Nadja van 't Hoff (11030720)
+"""
+
+def extraTijdslot(studentenLijst, zaalslotLijst):
+    "deze functie berekent de punten bij het gebruik van het extra tijdslot"
+
+    malusPunten = 0
+    
+    for zaal in zaalslot:
+        if tijdslot == 5 && inGebruik:
+            malusPunten += 50
+
+    return malusPunten
