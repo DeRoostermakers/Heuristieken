1c60abd723eefc7ac58aec657e245a34014a812e
Kenneth <kengoei@live.nl>
2018-05-15 10:13:12 +0200

    sequential

--- main.py
+++ main.py
@@ -30,9 +30,8 @@ tijdsloten = ["9.00-11.00", "11.00-13.00", "13.00-15.00", "15.00-17.00", "17.00-
 # maak een rooster object aan
 rooster = Rooster.Rooster(dagen, tijdsloten)
 
-<<<<<<< HEAD
+
 sequential(rooster)
-=======
 # geneticAlgorithm(rooster)
 
 # vul het rooster met vakken
@@ -44,7 +43,6 @@ rooster.vulRandom()
 
 
 # bereken de score
->>>>>>> 3432a0c77a8c4459c6da17e1d64b86428d643c49
 print(rooster.score())
 
 
@@ -53,15 +51,3 @@ print(rooster.score())
 # maak een random steekproef aan van x iteraties
 # iteraties = 51
 # randomSteekproef(rooster, iteraties)
-
-
-vanZaalNaarID = {"A1.04" : 0, "A1.06" : 1, "A1.08" : 2, "A1.10" : 3, "B0.201" : 4, "C0.110" : 5, "C1.112" : 6}
-"""
-import csv
-with open("rooster.csv", "w") as roosterCSV:
-     writer = csv.writer(roosterCSV, delimiter = ';')
-     writer.writerow(["RoomId", "RoomName", "Tijdslot", "Dagslot", "VakId", "Vaknaam", "Activiteit", "GroupId", "Placeholder", "Placeholder", "Studentnummers"])
-     for zaalslot in rooster.zaalslotenLijst:
-         if zaalslot.inGebruik != 0:
-             writer.writerow([vanZaalNaarID[zaalslot.naam], zaalslot.naam, zaalslot.tijdslot - 1, zaalslot.dag - 1, zaalslot.activiteit.vakId, rooster.vanIdNaarVak[zaalslot.activiteit.vakId], zaalslot.activiteit.soort, zaalslot.activiteit.groepnr, " " , " " , zaalslot.activiteit.welkeStud])
-"""
