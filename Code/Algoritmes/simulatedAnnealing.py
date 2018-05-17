30f68559d356be4036e4488c9063cff21e29ff5d
LinseyUvA <linseyschaap@tele2.nl>
2018-05-17 09:04:29 +0200

    update simulatedAnnealing

--- Code/Algoritmes/simulatedAnnealing.py
+++ Code/Algoritmes/simulatedAnnealing.py
@@ -17,47 +17,47 @@ def simulatedAnnealing(dagen, tijdsloten):
     rooster.vulRandom()
     score = rooster.score()
 
+    beginTemperatuur = 100
+    eindTemperatuur = 0
 
-    acceptatieKans = math.exp(-0.4)
-    print(acceptatieKans)
+    mutaties = 0
+    lijstScore = []
+    simulatedAnnelingRooster = []
+    for i in range(minIteraties):
 
+        # wissel twee willekeurige zaalsloten
+        indexZaalslot = random.sample(range(len(rooster.zaalslotenLijst)), 2)
+        randomZaalslot1 = rooster.zaalslotenLijst[indexZaalslot[0]]
+        randomZaalslot2 = rooster.zaalslotenLijst[indexZaalslot[1]]
+        randomZaalslot1.wissel(randomZaalslot2)
+        score2 = rooster.score()
+        lijstScore.append(score)
 
-    nummer = random.random()
-    print(nummer)
-    if acceptatieKans < nummer:
-        print("hoi")
+        if score2 > score:
+            score = score2
+            mutaties += 1
 
-    #
-    # mutaties = 0
-    # lijstScore = []
-    # simulatedAnnelingRooster = []
-    # for i in range(minIteraties):
-    #
-    #     # wissel twee willekeurige zaalsloten
-    #     indexZaalslot = random.sample(range(len(rooster.zaalslotenLijst)), 2)
-    #     randomZaalslot1 = rooster.zaalslotenLijst[indexZaalslot[0]]
-    #     randomZaalslot2 = rooster.zaalslotenLijst[indexZaalslot[1]]
-    #     randomZaalslot1.wissel(randomZaalslot2)
-    #     score2 = rooster.score()
-    #     lijstScore.append(score)
-    #
-    #     if score2 > score:
-    #         score = score2
-    #         mutaties += 1
-    #
-    #     else:
-    #         # nog steeds geaccepteerd toegestaan
-    #
-    #         # niet geaccepteerd
-    #         randomZaalslot2.wissel(randomZaalslot1)
-    #
-    # simulatedAnnelingRooster.append([rooster, score])
-    #
-    # aantalIteraties = []
-    # for i in range(minIteraties):
-    #     aantalIteraties.append(i)
-    #
-    # # print(aantalIteraties)
-    # # print(lijstScore)
-    # print(simulatedAnnelingRooster)
-    # return simulatedAnnelingRooster
+        else:
+            # nog steeds geaccepteerd toegestaan
+            temperatuur = beginTemperatuur-i*(beginTemperatuur-eindTemperatuur)/minIteraties
+            acceptatieKans = math.exp(temperatuur)
+            print(acceptatieKans)
+
+            nummer = random.random()
+            if acceptatieKans > nummer:
+                score = score2
+                mutaties += 1
+
+            # niet geaccepteerd
+            randomZaalslot2.wissel(randomZaalslot1)
+
+    simulatedAnnelingRooster.append([rooster, score])
+
+    aantalIteraties = []
+    for i in range(minIteraties):
+        aantalIteraties.append(i)
+
+    # print(aantalIteraties)
+    # print(lijstScore)
+    print(simulatedAnnelingRooster)
+    return simulatedAnnelingRooster
