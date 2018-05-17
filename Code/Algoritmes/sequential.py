c9d48b185f58d74a0144c71eb5c4651fae90bdcb
NadjaUvA <31736612+NadjaUvA@users.noreply.github.com>
2018-05-17 14:09:04 +0200

    update

--- Code/Algoritmes/sequential.py
+++ Code/Algoritmes/sequential.py
@@ -1,5 +1,6 @@
 import random
 import rooster as Rooster
+import math
 
 def sequential(dagen, tijdsloten):
 
@@ -76,3 +77,141 @@ def sequentialDos(dagen, tijdsloten):
     zalen = [[]  for i in range(len(rooster.zaalNaarID))]
     for zaalslot in rooster.zaalslotenLijst:
         zalen[rooster.zaalNaarID[zaalslot.naam]].append(zaalslot)
+
+
+    print(rooster.score())
+
+    beginTemperatuur = 100
+    eindTemperatuur = 0
+    minIteraties = 2000
+    score = rooster.score()
+    mutaties = 0
+    lijstScore = []
+    simulatedAnnelingRooster = []
+    for i in range(minIteraties):
+
+        # wissel twee willekeurige zaalsloten
+        indexZaalslot = random.sample(range(len(rooster.zaalslotenLijst)), 2)
+        randomZaalslot1 = rooster.zaalslotenLijst[indexZaalslot[0]]
+        randomZaalslot2 = rooster.zaalslotenLijst[indexZaalslot[1]]
+        randomZaalslot1.wissel(randomZaalslot2)
+        score2 = rooster.score()
+        lijstScore.append(score)
+
+        if score2 > score:
+            score = score2
+            mutaties += 1
+
+        else:
+            # nog steeds geaccepteerd toegestaan
+            temperatuur = beginTemperatuur-i*(beginTemperatuur-eindTemperatuur)/minIteraties
+            verkorting = score2 - score
+            acceptatieKans = math.exp(verkorting/temperatuur)
+            # print(acceptatieKans)
+
+            nummer = random.random()
+            if acceptatieKans > nummer:
+                score = score2
+                mutaties += 1
+
+            # niet geaccepteerd
+            randomZaalslot2.wissel(randomZaalslot1)
+
+    simulatedAnnelingRooster.append([rooster, score])
+
+    aantalIteraties = []
+    for i in range(minIteraties):
+        aantalIteraties.append(i)
+
+    # print(aantalIteraties)
+    print(lijstScore)
+
+
+def sequentialTres(dagen, tijdsloten):
+
+    rooster = Rooster.Rooster(dagen, tijdsloten)
+
+    # maak een random volgorde van de zaalslotenlijst
+    random.shuffle(rooster.zaalslotenLijst, random.random)
+
+    # maak een random volgorde van de activiteitenlijst
+    random.shuffle(rooster.activiteitenLijst, random.random)
+
+    # sorteer de activiteiten op het aantal studenten per activiteit
+    rooster.activiteitenLijst.sort(key = lambda x: x.nrStud, reverse = True)
+
+    # sorteer de zaalsloten op het aantal studenten per capaciteit
+    rooster.zaalslotenLijst.sort(key = lambda x: x.capaciteit, reverse = True)
+
+    j = 0
+    # voeg de activiteiten toe aan de zaalsloten, sla de zaalsloten van 17.00-19.00 over
+    for i in range(len(rooster.activiteitenLijst)):
+        while(rooster.zaalslotenLijst[j].tijdslot == 5):
+            j += 1
+
+        rooster.zaalslotenLijst[j].voegToe(rooster.activiteitenLijst[i])
+        j += 1
+
+    zalenTemp = [[]  for i in range(len(rooster.zaalNaarID))]
+    for zaalslot in rooster.zaalslotenLijst:
+        zalenTemp[rooster.zaalNaarID[zaalslot.naam]].append(zaalslot)
+
+    zalen = [[] for i in range(4)]
+    zalen[0].extend(zalenTemp[0])
+    zalen[1].extend(zalenTemp[1])
+    zalen[1].extend(zalenTemp[2])
+    zalen[2].extend(zalenTemp[3])
+    zalen[0].extend(zalenTemp[4])
+    zalen[3].extend(zalenTemp[5])
+    zalen[2].extend(zalenTemp[6])
+    print(zalen)
+    print("score na sequential stap 1: " + str(rooster.score()))
+
+
+    lijstScore = []
+    simulatedAnnelingRooster = []
+    for zaal in zalen:
+        beginTemperatuur = 100
+        eindTemperatuur = 0
+        minIteraties = 1000
+        score = rooster.score()
+        mutaties = 0
+
+        for i in range(minIteraties):
+            # wissel twee willekeurige zaalsloten
+            indexZaalslot = random.sample(range(len(rooster.zaalslotenLijst)), 2)
+            randomZaalslot1 = rooster.zaalslotenLijst[indexZaalslot[0]]
+            randomZaalslot2 = rooster.zaalslotenLijst[indexZaalslot[1]]
+            randomZaalslot1.wissel(randomZaalslot2)
+            score2 = rooster.score()
+            lijstScore.append(score)
+
+            if score2 > score:
+                score = score2
+                mutaties += 1
+
+            else:
+                # nog steeds geaccepteerd toegestaan
+                temperatuur = beginTemperatuur-i*(beginTemperatuur-eindTemperatuur)/minIteraties
+                verkorting = score2 - score
+                acceptatieKans = math.exp(verkorting/temperatuur)
+                # print(acceptatieKans)
+
+                nummer = random.random()
+                if acceptatieKans > nummer:
+                    score = score2
+                    mutaties += 1
+
+                # niet geaccepteerd
+                randomZaalslot2.wissel(randomZaalslot1)
+
+        simulatedAnnelingRooster.append([rooster, score])
+
+        aantalIteraties = []
+        for i in range(minIteraties):
+            aantalIteraties.append(i)
+
+    # print(aantalIteraties)
+    print(lijstScore)
+    print(len(lijstScore))
+    return [rooster, rooster.score()]
---
+++ Code/Algoritmes/test.py
@@ -0,0 +1,143 @@
+"""
+Algoritme dat een rooster zoekt door recombinatie en mutatie.
+
+Linsey Schaap (11036109), Kenneth Goei (11850701), Nadja van 't Hoff (11030720)
+"""
+
+import random
+import rooster as Rooster
+from operator import itemgetter
+
+def geneticAlgorithm(dagen, tijdsloten, groottePopulatie, aantalGeneraties):
+
+    # creëer populatie bestaande uit willekeurige roosters
+    populatie = []
+
+    for i in range(groottePopulatie):
+        burger = Rooster.Rooster(dagen, tijdsloten)
+        burger.vulRandom()
+        populatie.append([burger, burger.score()])
+
+    # iteraar door bepaald aantal generaties
+    for i in range(aantalGeneraties):
+        # maak generatie van kinderen ter grootte van de populatie
+        kinderen = []
+        for kind in range(groottePopulatie):
+
+            # selecteer willekeurig twee ouders uit de populatie
+            oudersIndex = random.sample(range(groottePopulatie), 2)
+            ouder1 = (populatie[oudersIndex[0]])[0]
+            ouder2 = (populatie[oudersIndex[1]])[0]
+
+
+            kind = Rooster.Rooster(dagen, tijdsloten)
+
+
+            ######################### RECOMBINATIE
+            # recombineer ouders door van ieder willekeurige, halve aantal activiteiten te gebruiken
+            aantalActiviteiten = len(ouder1.activiteitenLijst)
+
+            # kies de indeces helft van de activiteiten van ouder 1
+            indexActiviteiten = random.sample(range(aantalActiviteiten), round(aantalActiviteiten/2))
+
+            activiteitenOuder1 = []
+            activiteitenIdsOuder1 = []
+
+            # selecter de gekozen eerste helft van activiteiten van ouder 1
+            for i in range(len(indexActiviteiten)):
+                activiteit = ouder1.activiteitenLijst[indexActiviteiten[i]]
+                activiteitenOuder1.append(activiteit)
+                activiteitenIdsOuder1.extend(activiteit.activiteitId)
+
+            activiteitenOuder2 = []
+
+            # kies de activiteiten van ouder 2 die niet door ouder 1 zijn toegevoegd
+            for activiteit in ouder2.activiteitenLijst:
+                if activiteit.activiteitId not in activiteitenIdsOuder1:
+                    activiteitenOuder2.append(activiteit)
+
+            activiteitenKind = []
+            zaalslotenKind = []
+            legeZaalslotenOuder1 = []
+            legeZaalslotenId = []
+
+
+
+            # zoek de juiste zaalsloten bij de activiteit van ouder 1
+            for activiteit in activiteitenOuder1:
+                for zaalslot in ouder1.zaalslotenLijst:
+                    if zaalslot.activiteit == None:
+                        # controleren dat je niet elke ronde steeds de lege zaalsloten toevoegd
+                        if zaalslot not in legeZaalslotenOuder1:
+                            legeZaalslotenOuder1.append(zaalslot)
+                        if zaalslot.zaalslotId not in legeZaalslotenId:
+                            legeZaalslotenId.append(zaalslot.zaalslotId)
+
+                    # voeg zaalslot van activiteit van ouder 1 aan kind toe
+                    elif zaalslot.activiteit.activiteitId == activiteit.activiteitId:
+                        zaalslotenKind.append(zaalslot)
+                        activiteitenKind.append(activiteit)
+
+
+            # voeg activiteiten van ouder 2 toe
+            inTeRoosteren = []
+            for activiteit in activiteitenOuder2:
+
+                # rooster zaalslot wanneer deze leeg is
+                for zaalslot in ouder2.zaalslotenLijst:
+                    if zaalslot.zaalslotId in legeZaalslotenId:
+                        zaalslotenKind.append(zaalslot)
+
+                        # verwijder zaalslot uit lege lijst als hij in gebruik is
+                        for slot in legeZaalslotenId:
+                            if slot == zaalslot.zaalslotId:
+                                legeZaalslotenId.remove(slot)
+
+                    # hou bij welke activiteiten nog ingeroosterd moeten worden
+                    else:
+                        inTeRoosteren.append(activiteit)
+
+                activiteitenKind.append(activiteit)
+
+            # rooster overgebleven activiteiten in
+            i = 0
+            for activiteit in inTeRoosteren:
+                for zaalslot in legeZaalslotenOuder1:
+                    if zaalslot.zaalslotId not in legeZaalslotenId:
+                        zaalslot.voegToe(activiteit)
+                        zaalslotenKind.append(zaalslot)
+
+                activiteitenKind.append(activiteit)
+
+            kind.activiteitenLijst = activiteitenKind
+            kind.zaalslotenLijst = zaalslotenKind
+
+            # # muteer kind met 10% kans
+            # een = 1
+            # nummer = random.sample(range(10), een)
+            # if een in nummer:
+            #     # wissel twee willekeurige zaalsloten
+            #     indexZaalslot = random.sample(range(len(kind.zaalslotenLijst)), 2)
+            #     randomZaalslot1 = kind.zaalslotenLijst[indexZaalslot[0]]
+            #     randomZaalslot2 = kind.zaalslotenLijst[indexZaalslot[1]]
+            #     randomZaalslot1.wissel(randomZaalslot2)
+
+            print(kind.activiteitenLijst)
+            print(kind.zaalslotenLijst)
+            print(kind.vakkenLijst)
+
+            # voeg kind toe aan generatie van kinderen
+            kinderen.append([kind, kind.score()])
+
+        # sorteer totale populatie op scores
+        totalePopulatie = kinderen + populatie
+        populatieGesorteerd = sorted(totalePopulatie, key=itemgetter(1), reverse = True)
+
+        # selecteer beste 50 roosters uit de populatie
+        populatie = populatieGesorteerd[:groottePopulatie]
+
+    # selecteer beste rooster van laatste populatie
+    populatieGesorteerd = sorted(populatie, key=itemgetter(1))
+    besteScore = populatieGesorteerd[0]
+
+    return besteScore
