b95e6b6a7597e20c88c3a5d81951701bc6cb8e8b
Kenneth <kengoei@live.nl>
2018-05-28 11:01:33 +0200

    ip

--- main.py
+++ main.py
@@ -31,14 +31,22 @@ def main():
     dagen = ["maandag", "dinsdag", "woensdag", "donderdag", "vrijdag"]
     tijdsloten = ["9.00-11.00", "11.00-13.00", "13.00-15.00", "15.00-17.00", "17.00-19.00"]
 
-<<<<<<< HEAD
-    scoreLijst = []
-    for i in range(50):
-        rooster = Rooster.Rooster(dagen, tijdsloten)
-        nieuwRooster = sequentialTweevoudigeMinimalisatie(rooster, 4000, 5)
-        scoreLijst.append(nieuwRooster[0].score())
 
-    frequentieHistogram(scoreLijst)
+    scoreLijst = []
+    for i in range(3):
+        rooster = Rooster.Rooster(dagen, tijdsloten)
+        nieuwRooster = sequentialTweevoudigeMinimalisatie(rooster, 4, 5)
+        eind = hillClimberSteepestAscent(nieuwRooster[0], 2)
+        scoreLijst.append(eind.score())
+
+
+
+    with open("output.csv", "w") as output:
+        writer = csv.writer(output, lineterminator='\n')
+        for val in scoreLijst:
+            writer.writerow([val])
+
+    # frequentieHistogram(scoreLijst)
 
     # scoreLijst = []
     # for j in range(20):
@@ -130,90 +138,6 @@ def main():
 #         print("Bedankt! Hopelijk ben je tevreden met een rooster van " + str(scoreNieuw[len(scoreNieuw)-1]) + " punten. " +
 #         "Het rooster wordt voor je geprint.")
 #         visualiseer(tijdsloten, dagen, roosterNieuw)
-=======
-    rooster = Rooster.Rooster(dagen, tijdsloten)
-    rooster.vulRandom()
-
-    iteraties = 50
-    scoreLijst = []
-    for i in range(iteraties):
-        rooster.vulRandom()
-        nieuwRooster = hillClimberSteepestAscent(rooster, 200)
-        scoreLijst.append(nieuwRooster.score())
-        print("We zijn bij iteratie" + str(i))
-
-    frequentieHistogram(scoreLijst)
-
-    print("\nWELKOM BIJ HET INPLANNEN VAN DE LESROOSTERS\n")
-    uitvoer(rooster, dagen, tijdsloten)
-
-
-def uitvoer(rooster, dagen, tijdsloten):
-
-    algoritme = input("Welke algoritme wil je uitproberen?" +
-                      "\nJe kunt kiezen uit hillClimberStochastisch, hillClimberSteepestAscent," +
-                      "simulatedAnnealing, sequentialEenvoudigeMinimalisatie, " +
-                      "sequentialTweevoudigeMinimalisatie of geneticAlgorithm\n")
-
-    if algoritme == "geneticAlgorithm":
-        groottePopulatie = input("Hoe groot moet de populatie zijn?\n")
-        aantalGeneraties = input("Hoeveel generaties moeten er gemaakt worden?\n")
-        mutatieKans = input("Welke mutatiekans moet worden toegepast (getal tussen 0 en 1)?\n")
-
-    if (algoritme != "sequentialEenvoudigeMinimalisatie"
-        and algoritme != "geneticAlgorithm"):
-        aantalIteraties = input("Met hoeveel iteraties wil je dit algoritme uitvoeren?\n")
-
-    if algoritme == "simulatedAnnealing":
-        beginTemperatuur = input("begin temperatuur: ")
-        eindTemperatuur = input("eind temperatuur: ")
-        temperatuurFunctie = input("Wil je gebruik maken van een lineairFunctie, " +
-                                   "exponentieelFunctie of sigmoidalFunctie koelschema?\n")
-
-    if algoritme == "sequentialTweevoudigeMinimalisatie":
-        verschilZaal = input("Welk verschil in capaciteit verschil je tussen zalen voor de indeling?\n")
-
-    print("Cool, laten we " + algoritme + " uitvoeren! (Het kan even duren)")
-
-    if algoritme == "hillClimberStochastisch":
-        roosterNieuw, scoreNieuw = hillClimberStochastisch(rooster, int(aantalIteraties))
-    elif algoritme == "hillClimberSteepestAscent":
-        roosterNieuw, scoreNieuw = hillClimberSteepestAscent(rooster, int(aantalIteraties))
-    elif algoritme == "simulatedAnnealing":
-        if temperatuurFunctie == "lineairFunctie":
-            roosterNieuw, scoreNieuw = simulatedAnnealing(rooster, int(aantalIteraties),
-                                                          int(beginTemperatuur),
-                                                          int(eindTemperatuur),
-                                                          lineairFunctie)
-        elif temperatuurFunctie == "exponentieelFunctie":
-            roosterNieuw, scoreNieuw = simulatedAnnealing(rooster, int(aantalIteraties),
-                                                          int(beginTemperatuur),
-                                                          int(eindTemperatuur),
-                                                          exponentieelFunctie)
-        elif temperatuurFunctie == "sigmoidalFunctie":
-            roosterNieuw, scoreNieuw = simulatedAnnealing(rooster, int(aantalIteraties),
-                                                          int(beginTemperatuur),
-                                                          int(eindTemperatuur),
-                                                          sigmoidaFunctie)
-    elif algoritme == "sequentialEenvoudigeMinimalisatie":
-        roosterNieuw, scoreNieuw = sequentialEenvoudigeMinimalisatie(rooster)
-    elif algoritme == "sequentialTweevoudigeMinimalisatie":
-        roosterNieuw, scoreNieuw = sequentialTweevoudigeMinimalisatie(rooster, int(aantalIteraties), int(verschilZaal))
-    elif algoritme == "geneticAlgorithm":
-        roosterNieuw, scoreNieuw = geneticAlgorithm(rooster, dagen, tijdsloten,
-                                                    int(groottePopulatie),
-                                                    int(aantalGeneraties),
-                                                    float(mutatieKans))
-
-    print("Rooster: " + str(roosterNieuw) + "\nDit rooster heeft een score van " + str(scoreNieuw[len(scoreNieuw)-1]))
-    nogEenKeer = input("Wil je nog een algoritme op dit rooster uitproberen? (j/n)\n")
-    if nogEenKeer == "j":
-        return uitvoer(roosterNieuw, dagen, tijdsloten)
-    else:
-        print("Bedankt! Hopelijk ben je tevreden met een rooster van " + str(scoreNieuw[len(scoreNieuw)-1]) + " punten. " +
-        "Het rooster wordt voor je geprint.")
-        visualiseer(tijdsloten, dagen, roosterNieuw)
->>>>>>> 00a76a6ebc33555aa4fe93a8535bdb20351e8cec
 
 if __name__ == "__main__":
     main ()
